6556b8872ca3085312f9c1f9269a60e07be8f309
Guangyu Yan <yanxx630@umn.edu>
2018-12-10 03:03:33 -0600

    Dance

---
+++ config.h
@@ -0,0 +1,20 @@
+/**
+ */
+
+#ifndef config_h
+#define config_h
+
+// The file config.h.in is processed by cmake to produce config.h.  This
+// replaces strings of the form "at"CMAKE_VARIABLE_NAME"at" with the value
+// of the corresponding cmake variable, allowing us to pass directory paths
+// and other information configured with cmake into our C++ code.
+
+
+#define DATA_DIR_BUILD "/Users/Guangyu/Desktop/shared-upstream/dev/dance/data"
+#define DATA_DIR_INSTALL "/Users/Guangyu/Desktop/shared-upstream//share/a4-dance/data"
+
+
+#endif
+
+
+
